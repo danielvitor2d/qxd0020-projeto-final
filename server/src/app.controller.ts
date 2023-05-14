6a361bf0b1a0487097bf13777e393a4b984a3726
Daniel Vitor <ripsea.rpg321@gmail.com>
2023-05-14 15:47:10 -0300

    add phonenumber to user model

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,24 +1,13 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 import { PrismaService } from './database/prisma.service';
-import { CreateUser } from './dtos/create-user';
 
 @Controller()
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  async getHello(@Body() body: CreateUser) {
-    const { name } = body;
-
-    const member = await this.prisma.user.create({
-      data: {
-        name,
-      },
-    });
-
-    return {
-      member,
-    };
+  async getHello() {
+    return 'Hello';
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,16 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { PrismaService } from './database/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   await app.listen(3000, async () =>
     console.log('Server is running...', await app.getUrl()),
