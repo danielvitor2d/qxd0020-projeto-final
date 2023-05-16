096bffa1f402c5f73d7c83f4187ed649ec78263f
Daniel Vitor <ripsea.rpg321@gmail.com>
2023-05-16 01:50:13 -0300

    finish courses page

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Login from '../views/Login.vue'
-import Registration from '../views/Registration.vue'
+import Login from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
-import EditTest from '../views/EditTest.vue'
-import RecoverPassword from '../views/RecoverPassword.vue'
+import EditTest from '../views/EditTestView.vue'
+import CoursesView from '../views/CoursesView.vue'
+import Registration from '../views/RegistrationView.vue'
+import RecoverPassword from '../views/RecoverPasswordView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,14 +15,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // },
+    {
+      path: '/courses',
+      name: 'courses',
+      component: CoursesView
+    },
     {
       path: '/admin/edit-test',
       name: 'admin-edit-test',
---
+++ web/src/views/CoursesView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="pb-16 flex flex-col items-center">
+    <div class="w-full">
+      <img
+        class="w-full"
+        src="../assets/courses.svg"
+        alt="background"
+      />
+    </div>
+    <section class="w-9/12 mt-6 flex flex-col items-start justify-center">
+      <h1 class="font-bold text-3xl text-gray-800">Cursos do Campus de Quixadá</h1>
+
+      <h2 class="mt-5 font-bold text-xl text-gray-800">Engenharia de Software</h2>
+      <p class="mt-2 font-normal text-lg text-gray-700">
+        Mussum Ipsum, cacilds vidis litro abertis. Paisis, filhis, espiritis santis.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. 
+        Mussum Ipsum, cacilds vidis litro abertis. Cevadis im ampola pa arma uma pindureta.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Leite de capivaris, leite de mula manquis sem cabeça.Suco de cevadiss deixa as pessoas mais interessantis. 
+        Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Mé faiz elementum girarzis, nisi eros vermeio.
+      </p>
+
+      <h2 class="mt-5 font-bold text-xl text-gray-800">Ciência da Computação</h2>
+      <p class="mt-2 font-normal text-lg text-gray-700">
+        Mussum Ipsum, cacilds vidis litro abertis. Paisis, filhis, espiritis santis.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. 
+        Mussum Ipsum, cacilds vidis litro abertis. Cevadis im ampola pa arma uma pindureta.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Leite de capivaris, leite de mula manquis sem cabeça.Suco de cevadiss deixa as pessoas mais interessantis. 
+        Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Mé faiz elementum girarzis, nisi eros vermeio.
+      </p>
+      
+    </section>
+  </main>
+</template>
